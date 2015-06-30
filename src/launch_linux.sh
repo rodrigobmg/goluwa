86d37cb260795eaf5417d6cf4f03b3682a9ae187
CapsAdmin <eliashogstvedt@gmail.com>
2015-06-30 23:19:20 +0200

    put downloaded and generated files in data/ instead of .userdata/ and src/bin/ cleanup in init removed some unnecessary git files and settings desire libsndfile instead of require better reloading for vfs
    Former-commit-id: 72bac50661180160385540541d1f8495d9601752 [formerly f117ca25e9ee1f3ada943c982f330f9efbe56679] [formerly 10f5d2fa0d163a7922af6921730e284f6ba8bb78 [formerly 54160824dcf86746d008387e1fa5bd858bfedaf3]] [formerly 32aef0f4f3f98ed064288f9dcccb401ed7b8c632 [formerly 549bf565236b6d0dd2d36cebfc28999dfa99c2c9] [formerly 8ddf15586caff4b6e5114c68eda4c5aa155e74be [formerly 44be1a0b9849a4d166a6c449ef57280522f42e40]]]
    Former-commit-id: 710d3af28da78abda3633c1716e26ecdbafd84e1 [formerly 4b6b393f877ff971ff40873337548196716028a8] [formerly c7e1f81423dc5a5bd1bf2976d485ea94c323cfe8 [formerly cb3f63184da907a6de2139163846a010dfa12351]]
    Former-commit-id: 4fb85b15254526676e818fe0c8bcc62bf9fe8bb8 [formerly 73172bbc50e608f8904844b8f2bee9fc85b921ff]
    Former-commit-id: 9510a33731f6fb46250f73892184a32b187ade31

---
+++ src/launch_linux.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#figure out the binary architecture to use and download
+arch=unknown
+
+case $(uname -m) in
+	x86_64)  arch=x64 ;;
+	i[36]86) arch=x86 ;;
+	arm*)    arch=arm ;;
+esac
+
+# make sure we're in this bash's directory
+cd $( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd ../data/bin
+
+#if we don't have binaries get them from github
+if [ ! -f "linux_${arch}/luajit" ]; then
+	wget "https://github.com/CapsAdmin/goluwa/releases/download/linux-binaries/${arch}.zip" -O temp.zip
+	mkdir linux_${arch}
+	unzip temp.zip -d linux_${arch}
+	rm temp.zip
+fi
+
+cd ./linux_${arch}/
+
+#lookup shared libraries in "goluwa/data/bin/linux_${arch}/" first
+export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
+
+#i don't know if this is stupid or not but it's so i can execute luajt without 
+#the need for execute permissions on a non ext filesystem (like on a usb stick with fat32)
+if [ -e "/lib64/ld-linux-x86-64.so.2" ]; then
+	/lib64/ld-linux-x86-64.so.2 ./luajit ../../../src/lua/init.lua
+else
+	./luajit ../../../src/lua/init.lua
+fi
---
+++ src/launch_windows.ps1
@@ -0,0 +1,89 @@
+function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
+
+$src = @'
+    [DllImport("Kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+    [DllImport("User32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'@
+
+Add-Type -Name ConsoleUtils -Namespace Foo -MemberDefinition $src
+$hWnd = [Foo.ConsoleUtils]::GetConsoleWindow()
+
+if ($ENV:PROCESSOR_ARCHITECTURE -Match "64"){ $arch = "x64" } else { $arch = "x86" }
+$url = "https://github.com/CapsAdmin/goluwa/releases/download/windows-binaries/" + $arch + ".zip"
+$output_folder = [IO.Path]::GetFullPath($(PSScriptRoot) + "\..\data\bin\windows_" + $arch)
+
+if(!(Test-Path ($output_folder + "\luajit.exe")))
+{
+	[Foo.ConsoleUtils]::ShowWindow($hWnd, 1)
+	
+	if (!(Test-Path $output_folder))
+	{
+		New-Item -ItemType directory -Path $output_folder
+	}
+	
+	Clear-Host
+	
+	Write-Output "downloading binaries from $url to $output_folder"
+	
+	$download_location = (get-item $output_folder).parent.FullName + "\temp.zip"
+		
+	Remove-Item $download_location -ErrorAction SilentlyContinue -Confirm:$false -Recurse:$true
+	
+	if (Get-Command Invoke-WebRequest -CommandType cmdlet -errorAction SilentlyContinue)
+	{
+		#nicer download
+		Invoke-WebRequest $url -OutFile $download_location
+	}
+	else
+	{
+		$client = new-object System.Net.WebClient
+		$client.DownloadFile( $url, $download_location )	
+	}
+	
+	Write-Output "unzipping files"
+	
+	$shell = new-object -com shell.application
+	$zip = $shell.NameSpace($download_location)
+
+	foreach($item in $zip.items())
+	{
+		$shell.Namespace($output_folder).copyhere($item)
+	}
+	
+	Write-Output "removing leftover files"
+	
+	Remove-Item ($download_location) -ErrorAction SilentlyContinue -Confirm:$false -Recurse:$true
+	
+	Write-Output "launching"
+	
+	[Foo.ConsoleUtils]::ShowWindow($hWnd, 0)
+}
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $output_folder + "\luajit.exe"
+$pinfo.WorkingDirectory = $output_folder + "\"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments = "../../../src/lua/init.lua"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+#$p.WaitForExit()
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+
+Write-Host "======== stdout ========`n$stdout========================`n`n"
+Write-Host "======== stderr ========`n$stderr========================`n`n"
+Write-Host "exit code: " $p.ExitCode "`n`n"
+
+if ($stderr -or ($p.ExitCode -ne 0))
+{
+[Foo.ConsoleUtils]::ShowWindow($hWnd, 1)
+
+Write-Host "press any key to exit"
+$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+}
